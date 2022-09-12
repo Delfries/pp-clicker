d7728580b176841da4e88757f78b5eba5b8d1ba7
Delfries <joran.devries@gmail.com>
2022-09-12 13:55:18 +0200

    small penis not allowed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 penis_size = int(input('How long is your penis (inches): '))
 
-if int(penis_size) > 32:
-    print('Sorry your penis is too wide :(')
+if (penis_size) > 32:
+    print('Sorry your penis is too long :(')
+elif (penis_size) < 3:
+    print('Sorry your penis is too small :)') 
 else:
     print(f'Your penis will be {penis_size} wide')
