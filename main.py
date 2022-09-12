476f38760d00ac79e6679d243ff2a087a9f8cce8
grostaco <grostaco@gmail.com>
2022-09-12 18:50:41 +0700

    merge resolve

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
 print("how wide would you like your penis to be?")
-x = int(input())
-
-if x > 32:
-    print('Sorry your penis is too wide :(')
-    exit(0)
-
-print(f'Your penis will be {x} wide')
-=======
-print("How many inches long would you like your penis to be?")
 penis_size = int(input())
 
 if int(penis_size) > 32:
     print('Sorry your penis is too wide :(')
     exit(0)
-else:
-    print("Your penis will be " + str(penis_size) + " wide")
->>>>>>> daa254ea125ad9b47bdb95d4dcd26d1b282b5aa0
+
+print(f'Your penis will be {penis_size} wide')
