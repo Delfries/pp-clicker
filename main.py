585133e5166203aefb5be5aadb809857bb7b7d3c
grostaco <grostaco@gmail.com>
2022-09-12 18:56:35 +0700

    small penis bug

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ penis_size = int(input('How long is your penis (inches): '))
 
 if penis_size > 32:
     print('Sorry your penis is too long :(')
-elif penis_size < 3:
-    print('Sorry your penis is too small :)')
 elif penis_size <= 1:
     print('I am so sorry :()')
+elif penis_size < 3:
+    print('Sorry your penis is too small :)')
 else:
     print(f'Your penis will be {penis_size} wide')
