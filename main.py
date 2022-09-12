8c97807effb6751e049522630d7a76f44f9e3e25
grostaco <grostaco@gmail.com>
2022-09-12 18:32:52 +0700

    feat: penis length check

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print("how wide would you like your penis to be?") 
+print("how wide would you like your penis to be?")
 x = input()
+
+if x > 32:
+    print('Sorry your penis is too wide :(')
+    exit(0)
+
 print("Your penis will be " + x + " wide")
