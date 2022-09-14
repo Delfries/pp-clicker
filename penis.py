cc35f6d9c79a9bb122aff3d263f4205afc81c492
grostaco <grostaco@gmail.com>
2022-09-14 17:29:19 +0700

    feat: add abstract and measured penis

--- penis.py
+++ penis.py
@@ -4,6 +4,8 @@ Overengineered Penis Class
 No copyright, do whatever you want I am not responsible.
 """
 
+from abc import abstractmethod, ABCMeta
+
 PENIS_HEAD = """
     _____
    /  |  \\
@@ -15,25 +17,60 @@ PENIS_BALLS = """
 \\____/ \\____/
 """
 
-# Penis class
 
+class AbstractPenis(metaclass=ABCMeta):
+    """
+    Abstract Class Penis
 
-class Penis:
+    For implementing various penises
+    """
+
     def __init__(self, length: int) -> None:
         self._length = length
 
     @property
-    def length(self) -> int:
-        return self._length
+    @abstractmethod
+    def length(self) -> int: pass
 
     @length.setter
-    def length(self, new_length: int):
-        if new_length < 1:
-            raise ValueError("Penis length cannot be non-existent or inverted")
-        self._length = new_length
+    @abstractmethod
+    def length(self, new_length: int): pass
 
     def shaft(self) -> str:
         return self.length * '   |     |\n'
 
     def __str__(self) -> str:
         return PENIS_HEAD + self.shaft()[:-1] + PENIS_BALLS
+
+
+class MeasuredPenis(AbstractPenis):
+    """
+    MeasuredPenis
+
+    Penis with a length attachment on `__str__`
+    """
+    @property
+    def length(self) -> int:
+        return self._length
+
+    @length.setter
+    def length(self, new_length: int):
+        self._length = new_length
+
+    def shaft(self) -> str:
+        buf = ['   |     |'] * self.length
+        for (i, segment) in enumerate(buf):
+            if i == self.length // 2:
+                segment += f'     +--- {self.length} inches'
+            elif i == 0:
+                segment += ' ----+'
+            elif i == self.length - 1:
+                segment += ' ----+'
+            else:
+                segment += '     |'
+            buf[i] = segment
+        return '\n'.join(buf)
+
+
+if __name__ == '__main__':
+    print(MeasuredPenis(5))
