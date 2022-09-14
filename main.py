3c07626856f22dca1ed3875414bf74fdf236cc6a
grostaco <grostaco@gmail.com>
2022-09-14 17:30:58 +0700

    feat: use penis class

--- main.py
+++ main.py
@@ -2,32 +2,10 @@
 from os import system
 from colorama import Fore
 import win32api
-import time
+
+from penis import MeasuredPenis
 
 cum = 0
-
-def draw_penis(penis_size: int, color: str):
-    print('\n\n=====PENIS PREVIEW=====\n\n')
-
-    color = getattr(Fore, penis_color.upper())
-    print(color)
-
-    print('      _____')
-    print('     /  |  \\')
-    print('     \\     /')
-    for i in range(0, penis_size):
-        print(color + '     |     |', end='')
-        if i == 0 or i == penis_size - 1:
-            print(Fore.WHITE + ' ---+')
-        elif i == penis_size // 2:
-            print(Fore.WHITE + f'    +--- {penis_size} inches')
-        else:
-            print(Fore.WHITE + '    |')
-    print(color, end='')
-    print('   ____   ____')
-    print('  /    \\ /    \\  ')
-    print('  \\____/ \\____/')
-
 
 penis_size = int(input(Fore.WHITE + 'How long is your penis (inches): '))
 
@@ -44,12 +22,14 @@ penis_color = (input('What color is your penis?:'))
 
 
 last_state = 1
-draw_penis(penis_size, penis_color)
+penis = MeasuredPenis(penis_size)
+print(penis)
+
 while True:
     if (state := win32api.GetKeyState(1)) != last_state:
         last_state = state
         if state < 0:
-            penis_size += 1
+            penis.length += 1
             system('cls')
-            draw_penis(penis_size, penis_color)
+            print(penis)
             cum = cum + 1
